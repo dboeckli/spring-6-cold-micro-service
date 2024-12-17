56bbfb4604aa354e8dd30e46ed3f880457c0acbe
Dominique Boeckli <dboeckli@gmail.com>
2024-12-17 06:35:10 +0100

    setup project

---
+++ src/main/java/ch/dboeckli/springframeworkguru/spring6coldmicroservice/Spring6ColdMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.dboeckli.springframeworkguru.spring6coldmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ColdMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ColdMicroServiceApplication.class, args);
+    }
+
+}
