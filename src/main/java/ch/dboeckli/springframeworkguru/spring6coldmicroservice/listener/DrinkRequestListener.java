07a87510366b6420ad8d57a5f4ef801bcfc2d34c
adm-boeckli <dboeckli@gmail.com>
2024-12-20 17:08:51 +0100

    first implementation

--- src/main/java/ch/dboeckli/springframeworkguru/spring6coldmicroservice/listener/DrinkRequestListener.java
+++ src/main/java/ch/dboeckli/springframeworkguru/spring6coldmicroservice/listener/DrinkRequestListener.java
@@ -19,7 +19,7 @@ public class DrinkRequestListener {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    @KafkaListener(groupId = "IceColdListener", topics = KafkaConfig.DRINK_REQUEST_ICE_COLD_TOPIC)
+    @KafkaListener(groupId = "IceColdListener", topics = KafkaConfig.DRINK_REQUEST_COLD_TOPIC)
     public void listenDrinkRequest(DrinkRequestEvent event) {
         log.info("### I am listening - Cold drink request");
 
