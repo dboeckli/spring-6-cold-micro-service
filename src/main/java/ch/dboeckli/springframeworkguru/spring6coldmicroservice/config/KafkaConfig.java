f9e57c034ca74302e01898e31f37378db12110ae
Dominique Boeckli <dboeckli@gmail.com>
2024-12-21 08:59:38 +0100

    fix name

--- src/main/java/ch/dboeckli/springframeworkguru/spring6coldmicroservice/config/KafkaConfig.java
+++ src/main/java/ch/dboeckli/springframeworkguru/spring6coldmicroservice/config/KafkaConfig.java
@@ -1,6 +1,6 @@
 package ch.dboeckli.springframeworkguru.spring6coldmicroservice.config;
 
 public class KafkaConfig {
-    public static final String DRINK_REQUEST_COLD_TOPIC = "drink.request.icecold";
+    public static final String DRINK_REQUEST_COLD_TOPIC = "drink.request.cold";
     public static final String DRINK_PREPARED_TOPIC = "drink.prepared";
 }
